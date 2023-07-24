08c6b0bfb0912a9fc33943adaecfe1adc59355da
Joshua Einhoff <83461466+joshuaeinhoff@users.noreply.github.com>
2023-07-24 11:55:11 +0900

    Add shell scripts for USB Handling

---
+++ KillUSB.sh
@@ -0,0 +1,2 @@
+sudo sh -c 'echo "0" > /sys/bus/usb/devices/1-2/power/control'
+echo USB disabled
---
+++ ReviveUSB.sh
@@ -0,0 +1,2 @@
+sudo sh -c 'echo "auto" > /sys/bus/usb/devices/1-2/power/control'
+echo USB enabled
